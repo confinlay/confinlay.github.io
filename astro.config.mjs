6a9945d37521f77b28016a593dfcc556abb45a98
Conor Finlay <106957733+confinlay@users.noreply.github.com>
2025-02-03 11:30:15 +0000

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
-  site: "https://YOUR_GITHUB_USERNAME.github.io",
+  site: "https://confinlay.github.io",
   base: "/portfolio-website",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
