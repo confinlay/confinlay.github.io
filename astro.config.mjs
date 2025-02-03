e97266e48527908cf95fd7ba46463164f956db41
Conor Finlay <106957733+confinlay@users.noreply.github.com>
2025-02-03 11:04:04 +0000

    Configure GitHub Pages Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://YOUR_GITHUB_USERNAME.github.io",
+  base: "/portfolio-website",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkMath],
